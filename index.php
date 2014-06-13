20d92a880cf611686a11451f14a8409bc8768add
edward sharp <edward@edwardsharp.net>
2014-06-13 19:58:41 -0400

    logshit

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+require('vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+// add records to the log
+$log->addWarning("ZOMG THIS IS ADD LOGG");
+
+// don't forget to greet the world!
+//echo "Hello World!";
+
+
+
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
@@ -7,7 +24,6 @@ function pg_connection_string() {
   //  postgres://xoxyxywvhpenav:RvrEMtAH5IbjJR9iKIdqUIk1T-@ec2-54-225-101-202.compute-1.amazonaws.com:5432/df0g26t452pud4
 
 }
-
 
  
 # Establish db connection
