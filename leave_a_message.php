6657019b29547109aac751a26bdc8c381008d0be
edward sharp <edward@edwardsharp.net>
2014-06-14 01:46:20 -0400

    mailbox back!
    Signed-off-by: edward sharp <edward@edwardsharp.net>

---
+++ leave_a_message.php
@@ -0,0 +1,27 @@
+<?php
+
+require "Services/Twilio.php";
+include "messages.php";
+
+if (strlen($_REQUEST['exten'])) {
+	$exten = $_REQUEST['exten'];
+	$mailbox = getMailbox($exten);
+
+	//output TwiML to record the message
+	$response = new Services_Twilio_Twiml();
+	$response->say('will record ' . $mailbox['desc']);
+	$response->record(
+		array(
+			'action' => "handle_message.php?exten=$exten",
+			'maxLength' => '120')
+	);
+
+	// record will post to this url if it receives a message
+	// otherwise it falls through to the next verb
+	$response->gather()
+		->say("Sorry, failure seems eminent. Press any key to try again");
+
+	print $response;
+}
+
+?>
---
+++ message_menu.php
@@ -0,0 +1,42 @@
+<?php
+
+require "Services/Twilio.php";
+include "messages.php";
+
+if (strlen($_REQUEST['exten'])){
+	$exten=$_REQUEST['exten'];
+} else {
+	$response = new Services_Twilio_Twiml();
+	$response->say("Sorry, Failure. Oh golly... ");
+	die((string) $response);
+}
+
+if (strlen($_REQUEST['Digits'])){
+	$digits = $_REQUEST['Digits'];
+	if ($digits == 1 || $digits == 2) {
+		$messages = getMessages($exten, $digits - 1);
+		$location = "location: listen.php?exten=$exten&messages="
+			. urlencode(implode(",",$messages));
+		header($location);
+		exit();
+	} else {
+		$error=true;
+	}
+}
+
+$messages = getMessages($exten, 0);
+$new_msgs = count($messages);
+
+$response = new Services_Twilio_Twiml();
+
+if($error)
+	$response->say('Unable to process that extension');
+
+$gather = $response->gather(array("numDigits" => "1"));
+$gather->say("$new_msgs new");
+$gather->say("To listen press 1.");
+$gather->say("saved messages press 2.");
+$response->say("ya'll come back now, ya hear?");
+print $response;
+
+?>
