bebc3dc964c904f06223f99fb57032103f66e352
edward sharp <edward@edwardsharp.net>
2014-06-14 00:52:28 -0400

    fix it up, fix it up, wayyyy up

---
+++ browser-record.php
@@ -0,0 +1,76 @@
+<?php // @start snippet
+include 'Services/Twilio/Capability.php';
+$accountSid = 'AC828cb7e80f68cbb54b74e3ecb990bdf6';
+$authToken  = 'fff5edb02b98081f968436dc8db33434';
+$token = new Services_Twilio_Capability($accountSid, $authToken);
+$token->allowClientOutgoing('APe50e37e24df1d6f7dbbc626f7ab9a722'); // @end snippet
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			Twilio Client Call
+		</title>
+		<!-- @start snippet -->
+		<script type="text/javascript" src="//static.twilio.com/libs/twiliojs/1.1/twilio.min.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+		<script type="text/javascript">
+		var connection=null;
+		$(document).ready(function(){
+			Twilio.Device.setup("<?php echo $token->generateToken();?>",{"debug":true});
+			$("#call").click(function() {  
+				Twilio.Device.connect();
+			});
+			$("#hangup").click(function() {  
+  				connection.sendDigits("#");
+			});
+
+			Twilio.Device.ready(function (device) {
+				$('#status').text('system is ready, yo!');
+			});
+
+			Twilio.Device.offline(function (device) {
+				$('#status').text('offline! #sorry');
+			});
+
+			Twilio.Device.error(function (error) {
+				$('#status').text(error);
+			});
+
+			Twilio.Device.connect(function (conn) {
+				connection=conn;
+				$('#status').text("it is live, yo!");
+				$('#status').css('color', '#ff660');
+				toggleCallStatus();
+			});
+
+			Twilio.Device.disconnect(function (conn) {
+				$('#status').html('recording ended!<br/><a href="show_recordings.php">view recording list</a>');
+				$('#status').css('color', 'black');
+				toggleCallStatus();
+			});
+			
+			function toggleCallStatus(){
+				$('#call').toggle();
+				$('#hangup').toggle();
+			}
+		});
+		</script>
+		<!-- @end snippet -->
+
+	</head>
+	<body>
+		<div align="center">
+		<!-- @start snippet -->
+			<h3>Create a Recording</h3>
+			<input type="button" id="call" value="start recording"/>
+			<input type="button" id="hangup" value="stop recording" style="display:none;"/>
+			<div id="status">
+				yo, it's offline rightnow.
+			</div>
+		<!-- @end snippet -->
+		</div>
+
+	</body>
+</html>
---
+++ show_recordings.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			LISTEN UP (TO ZEE FILEZ)
+		</title>
+		<body>
+			<h1>LISTEN UP (TO ZEE FILE RECORDINGZ)</h1>
+		<?php
+    // Include the PHP TwilioRest library 
+	include 'Services/Twilio.php';
+    
+    // Twilio REST API version 
+    $ApiVersion = "2010-04-01";
+    
+    // Set our AccountSid and AuthToken 
+	$accountSid = 'AC828cb7e80f68cbb54b74e3ecb990bdf6';
+	$authToken  = 'fff5edb02b98081f968436dc8db33434';
+
+	// @start snippet
+    // Instantiate a new Twilio Rest Client 
+	$client = new Services_Twilio($accountSid, $authToken);
+	echo ("<table>");
+	foreach($client->account->recordings as $recording) {
+  		echo "<tr><td>{$recording->duration} seconds</td> ";
+  		echo "<td><audio src=\"https://api.twilio.com/2010-04-01/Accounts/$accountSid/Recordings/{$recording->sid}.mp3\" controls preload=\"auto\" autobuffer></audio></td>";
+  		echo "<td>{$recording->date_created}</td>";
+  		echo "<td>{$recording->sid}</td></tr>";
+	}
+	echo ("<table>");
+	// @end snippet
+    ?>
+	</body>
+</html>
