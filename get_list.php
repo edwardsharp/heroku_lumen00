d6995c10bda8cc170351b53547bb9fd3bf60a996
edward sharp <edward@edwardsharp.net>
2014-06-13 23:44:54 -0400

    uppp

--- get_list.php
+++ get_list.php
@@ -3,13 +3,30 @@
 require('/path/to/twilio-php/Services/Twilio.php'); 
  
 $account_sid = 'AC828cb7e80f68cbb54b74e3ecb990bdf6'; 
-$auth_token = '[AuthToken]'; 
+$auth_token = 'a070bfc127cddd747c0c90397c834710'; 
 $client = new Services_Twilio($account_sid, $auth_token); 
  
 $recordings = $client->account->recordings->getIterator(0, 50, array(   
 )); 
  
-foreach ($recordings as $recording) { 
+foreach ($recordings as $recording) {
+	echo " - - - - - - -"	 
 	echo $recording->sid; 
 }
+
+
+
+
+
+
+$client = new Services_Twilio($account_sid, $auth_token); 
+ 
+$calls = $client->account->calls->getIterator(0, 50, array(      
+)); 
+ 
+foreach ($calls as $call) { 
+	echo " # # # # # # # # "
+	echo $call->sid; 
+}
+
 ?>
