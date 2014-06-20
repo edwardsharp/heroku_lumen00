cf870115bf953394196c093549055e3f580500b1
edward sharp <edward@edwardsharp.net>
2014-06-20 02:33:12 -0400

    sofun
    Signed-off-by: edward sharp <edward@edwardsharp.net>

--- t/sms-reply.php
+++ t/sms-reply.php
@@ -1,12 +1,15 @@
 <?php
-require('vendor/autoload.php');
+require('../vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('appdebug');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+$log->addWarning("_REQUEST ".$_REQUEST);
+
 
 $from = $_REQUEST['From'];
-
-
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
-$log->addWarning("REQUEST ".$_REQUEST);
 
 if( isset($_GET['vardump']) ) {
   var_dump($_REQUEST);
