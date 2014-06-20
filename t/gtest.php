55d91d35629dcc1bd34494a1512c16d586fcda50
edward sharp <edward@edwardsharp.net>
2014-06-19 22:10:11 -0400

    testing
    Signed-off-by: edward sharp <edward@edwardsharp.net>

--- t/gtest.php
+++ t/gtest.php
@@ -10,7 +10,15 @@
   
  //    header("content-type: text/xml");
 		
-		$data = str_getcsv(file_get_contents('https://docs.google.com/a/edwardsharp.net/spreadsheets/d/1KuyXu3OiXuKD7hScO_gJlx5cS_74oMPvGrwwawm62ck/export?gid=0&format=csv'));
+function getURL($url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $ret = curl_exec($ch);
+    curl_close($ch);
+    return $ret;
+}
+
+		$data = str_getcsv(getURL('https://docs.google.com/a/edwardsharp.net/spreadsheets/d/1KuyXu3OiXuKD7hScO_gJlx5cS_74oMPvGrwwawm62ck/export?gid=0&format=csv'));
 
 		var_dump($data);
 
