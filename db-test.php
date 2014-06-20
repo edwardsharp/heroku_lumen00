f2eea388f90247bc4241286c918df0cf4455159a
edward sharp <edward@edwardsharp.net>
2014-06-19 22:27:35 -0400

    testz
    Signed-off-by: edward sharp <edward@edwardsharp.net>

--- db-test.php
+++ db-test.php
@@ -12,22 +12,27 @@ $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 $log->addWarning("ZOMG THIS IS ADD LOGG");
  
 
- $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"],1);
 
-  mysql_connect($server, $username, $password);
+    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-  mysql_select_db($db);
- 
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    mysql_connect($server, $username, $password);
+
+
+    mysql_select_db($db);
+
+
 	if (!$db) {
 	    echo "err?";
 	    exit;
 	}else{
 		echo "ZOMG,YAY!";
+		var_dump($db);
 	}
 
 
